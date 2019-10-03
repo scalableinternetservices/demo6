1618d10bc8e2edf822b2a36efd1eeaf540a7cafe
Bryce Boe <bbzbryce@gmail.com>
2019-10-03 09:04:57 -0700

    Modify submission migration to make title and url required

--- db/migrate/20191003160343_create_submissions.rb
+++ db/migrate/20191003160343_create_submissions.rb
@@ -1,8 +1,8 @@
 class CreateSubmissions < ActiveRecord::Migration[6.0]
   def change
     create_table :submissions do |t|
-      t.string :title
-      t.string :url
+      t.string :title, null: false
+      t.string :url, null: false
       t.references :community, null: false, foreign_key: true
 
       t.timestamps
