3c722fef4140148dfc20de25dc471a9657be0bfe
Bryce Boe <bbzbryce@gmail.com>
2019-10-03 08:57:24 -0700

    Modify community migration to make name required

--- db/migrate/20191003155132_create_communities.rb
+++ db/migrate/20191003155132_create_communities.rb
@@ -1,7 +1,7 @@
 class CreateCommunities < ActiveRecord::Migration[6.0]
   def change
     create_table :communities do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
