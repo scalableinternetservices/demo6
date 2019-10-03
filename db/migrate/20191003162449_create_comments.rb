f0855de938e912dc2bc297b5203eb25937f977b7
Bryce Boe <bbzbryce@gmail.com>
2019-10-03 10:04:35 -0700

    Modify Comment migration...
    * make message required
    * properly link parent_id foreign key and make optional

--- db/migrate/20191003162449_create_comments.rb
+++ db/migrate/20191003162449_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.text :message
+      t.text :message, null: false
       t.references :submission, null: false, foreign_key: true
-      t.references :parent, null: false, foreign_key: true
+      t.references :parent, null: true, foreign_key: { to_table: :comments }
 
       t.timestamps
     end
