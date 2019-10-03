6cef3aa703d4ceda102684f024866c1f15bda477
Bryce Boe <bbzbryce@gmail.com>
2019-10-03 08:47:04 -0700

    Prevent assets, helper, and jbuilder files from being created with generator

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module Demo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators.assets = false
+    config.generators.helper = false
+    config.generators.jbuilder = false
   end
 end
