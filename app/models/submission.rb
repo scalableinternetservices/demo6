c9dd27518f0785d58a27335128deadbbd2f965fd
Bryce Boe <bbzbryce@gmail.com>
2019-10-03 21:57:41 -0700

    Add validators to Submission

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,6 @@
 class Submission < ApplicationRecord
   belongs_to :community
   has_many :comments, dependent: :destroy
+  validates :title, length: { minimum: 5 }
+  validates :url, url: true
 end
--- test/models/submission_test.rb
+++ test/models/submission_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class SubmissionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validates title length' do
+    submission = Submission.new(community: communities(:one), title: 'abcd', url: 'https://google.com')
+    assert_predicate submission, :invalid?
+    assert_equal({ title: ['is too short (minimum is 5 characters)'] }, submission.errors.messages)
+  end
+
+  test 'validates url' do
+    submission = Submission.new(community: submissions(:one).community, title: 'abcde', url: 'https:/google.com')
+    assert_predicate submission, :invalid?
+    assert_equal({ url: ['is not a valid URL'] }, submission.errors.messages)
+  end
 end
