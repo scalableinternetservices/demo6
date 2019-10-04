6fb436005c0bfc4849b6977c00dd82ca21ff059f
Bryce Boe <bbzbryce@gmail.com>
2019-10-03 22:05:08 -0700

    Add validators to Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :parent, class_name: 'Comment', optional: true
   belongs_to :submission
+
+  validates :message, length: { minimum: 1 }
+  validates :parent, presence: { unless: -> { parent_id.blank? } }
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validates message length' do
+    comment = Comment.new(message: '', submission: submissions(:one))
+    assert_predicate comment, :invalid?
+    assert_equal({ message: ['is too short (minimum is 1 character)'] }, comment.errors.messages)
+  end
+
+  test 'validates parent' do
+    comment = Comment.new(message: 'a', parent_id: -1, submission: submissions(:one))
+    assert_predicate comment, :invalid?
+    assert_equal({ parent: ["can't be blank"] }, comment.errors.messages)
+  end
 end
